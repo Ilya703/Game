767721d7077f8b3867e4c82b407ea673dc01b19b
Ilya <ilya.skobelev.04@mail.ru>
2023-02-18 20:07:22 +0300

    feat: add README

---
+++ README.md
@@ -0,0 +1,33 @@
+# Game 🎮
+
+The Game project is a JavaScript quiz game. It was developed as a base component for the Adapt authoring tool platform and was created in collaboration with LightsOn and Sber
+
+### 🌐 [Site deployment](https://ilya703.github.io/Game/)
+
+### 🚀 Installation
+
+1. Clone the repository:
+
+```cmd
+git clone https://github.com/Ilya703/Game.git
+```
+
+2. Install the dependencies:
+
+```cmd
+npm install
+```
+
+3. Start the application:
+
+```cmd
+Just open index.html file in your browser
+```
+
+### 🛠️ Built With:
+
+* HTML5
+* CSS3
+* JavaScript
+* JQuery
+
--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
 let i = 1;
 let count = 0;
-let set = ['Домашний театр','Гардероб официантов','Шубохранилище','Аквадискотека','Компьютерный клуб'];
+let set = ['Question 1','Question 2','Question 3','Question 4','Question 5'];
 $('.button_1').on('click',function(){
 	let a = document.getElementById('text').innerHTML;
-	if (a == 'Домашний театр'){
+	if (a == 'Question 1'){
 		count += 1;
-	} else if (a == 'Гардероб официантов'){
+	} else if (a == 'Question 2'){
 		count += 1;
-	} else if (a == 'Шубохранилище'){
+	} else if (a == 'Question 3'){
 		count += 0;		
-	} else if (a == 'Аквадискотека'){
+	} else if (a == 'Question 4'){
 		count += 1;	
-	} else if (a == 'Компьютерный клуб'){
+	} else if (a == 'Question 5'){
 		count += 0;		
 	};
 	document.getElementById('text').innerHTML = set[i]
@@ -20,20 +20,20 @@ $('.button_1').on('click',function(){
 	if (i == 6){
 	document.getElementById('text').innerHTML = ''
 	$('.block_in').css({'opacity': 1,'z-index': 2});
-	document.getElementById('num').innerHTML = `${count} из 5`;
+	document.getElementById('num').innerHTML = `${count} out of 5`;
 	}
 });
 $('.button_2').on('click',function(){
 	let a = document.getElementById('text').innerHTML;
-	if (a == 'Домашний театр'){
+	if (a == 'Question 1'){
 		count += 0;
-	} else if (a == 'Гардероб официантов'){
+	} else if (a == 'Question 2'){
 		count += 0;
-	} else if (a == 'Шубохранилище'){
+	} else if (a == 'Question 3'){
 		count += 1;		
-	} else if (a == 'Аквадискотека'){
+	} else if (a == 'Question 4'){
 		count += 0;	
-	} else if (a == 'Компьютерный клуб'){
+	} else if (a == 'Question 5'){
 		count += 1;		
 	};
 	document.getElementById('text').innerHTML = set[i]
@@ -41,17 +41,17 @@ $('.button_2').on('click',function(){
 	i += 1
 	if (i == 6){
 		if (count < 3){
-		document.getElementById('phrase').innerHTML = 'Мне удалось заблудиться во «дворце Путина». Ищите меня на складе грязи!';
+		document.getElementById('phrase').innerHTML = "You did pretty well on the quiz, but there's always room for improvement";
 		} else {
-			document.getElementById('phrase').innerHTML = 'Я знаю, как устроен «дворец Путина». Ищите меня в spa-капсуле';
+			document.getElementById('phrase').innerHTML = 'Congratulations, you did an excellent job on the quiz!';
 		}
 	document.getElementById('text').innerHTML = ''
 	$('.block_in').css({'opacity': 1,'z-index': 2});
-	document.getElementById('num').innerHTML = `${count} из 5`;
+	document.getElementById('num').innerHTML = `${count} out of 5`;
 	}
 });
 $('.once_more').on('click',function(){
-	document.getElementById('text').innerHTML = 'Домашний театр';
+	document.getElementById('text').innerHTML = 'Question 1';
 	i = 1;
 	count = 0;
 	document.getElementById('count').innerHTML = 0
